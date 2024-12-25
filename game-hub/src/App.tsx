1fc6508af3b239c453fc04183b4c9ea94a72a2b1
Aaron Mubiruan <mubiruaron2003@gmail.com>
2024-12-25 16:25:31 -0400

    Built Initial Responsive layout

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,14 +1,40 @@
-import { Button } from "@/components/ui/button"
+import { Grid, GridItem, useBreakpointValue } from "@chakra-ui/react";
 
 function App() {
-  
+  // useBreakpointValue is used to set showAside to true for lg and larger breakpoints, and false for smaller breakpoints.
+  const showAside = useBreakpointValue({ base: false, lg: true });
 
   return (
     <>
-      <h1>Game Hub</h1>
-      <Button colorPalette='blue'>Click me</Button>
+      <Grid
+        templateAreas={{
+          //Defining two different layouts for different screen sizes
+          //One for mobile devices and for large devices with screens wider than 1024px
+          //Mobile layout, we only have nav and main areas we do not have aside
+          base: `
+          "nav"
+          "main"
+        `,
+          //Large layout, we have all three areas , wider than 1024px
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        {/* To make sure the aside is only rendered in large devices */}
+        {/* The Aside component is conditionally rendered based on the value of showAside. */}
+        {showAside && (
+          <GridItem area="aside" bg="gold">
+            Aside
+          </GridItem>
+        )}
+        <GridItem area="main" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
