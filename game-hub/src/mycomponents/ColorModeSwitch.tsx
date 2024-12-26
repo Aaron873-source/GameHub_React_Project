2f3448bdf4e9684b86f85f7ca0b5746e95343d9d
Aaron Mubiruan <mubiruaron2003@gmail.com>
2024-12-25 22:12:39 -0400

    Built a component for controlling my colorModeSwitch from darkMde to light mode of the page

---
+++ game-hub/src/mycomponents/ColorModeSwitch.tsx
@@ -0,0 +1,22 @@
+import { useColorMode } from "@/components/ui/color-mode";
+import { Switch } from "@/components/ui/switch";
+import { HStack, Text } from "@chakra-ui/react";
+
+const ColorModeSwitch = () => {
+  // const [checked, setChecked] = useState(false)
+
+  const { toggleColorMode, colorMode } = useColorMode();
+
+  return (
+    <HStack>
+      <Switch
+        colorPalette="green"
+        checked={colorMode === "dark"}
+        onChange={toggleColorMode}
+      />
+      <Text>Dark Mode</Text>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;
