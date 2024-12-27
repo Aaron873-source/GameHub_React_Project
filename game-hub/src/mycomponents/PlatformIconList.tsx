137c6f3de1e57b6091820bb455ee930a27d55d01
Aaron Mubiruan <mubiruaron2003@gmail.com>
2024-12-27 01:45:44 -0400

    Displaying platform Icons of the gaming platforms each game is playable

---
+++ game-hub/src/mycomponents/PlatformIconList.tsx
@@ -0,0 +1,45 @@
+import { Platform } from "@/hooks/useGames";
+import { HStack } from "@chakra-ui/react";
+import React from "react";
+import { BsGlobe } from "react-icons/bs";
+import {
+  FaAndroid,
+  FaApple,
+  FaLinux,
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
+} from "react-icons/fa";
+import { IconType } from "react-icons/lib";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack color={"gray.500"} marginY={1}>
+      {platforms.map((platform) => (
+        <span key={platform.id}>
+          {React.createElement(iconMap[platform.slug])}
+        </span>
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
