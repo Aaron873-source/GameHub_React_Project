8fad0f564e8f6e53dbe6fab52f76c1c2dd91537f
Aaron Mubiruan <mubiruaron2003@gmail.com>
2024-12-27 15:18:52 -0400

    Showing a spinner and Genre Loading text while fetching the genres before they are displayed

--- game-hub/src/mycomponents/GenreList.tsx
+++ game-hub/src/mycomponents/GenreList.tsx
@@ -1,21 +1,39 @@
 import useGenres from "@/hooks/useGenres";
 import getCroppedImageUrl from "@/services/image-url";
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 /**
  * GenreList component fetches and displays a list of genres.
- * 
+ *
  * This component uses the `useGenres` hook to fetch genre data and displays each genre
  * in a list item with an image and name. The list is rendered using Chakra UI components.
- * 
+ *
  * @component
  * @example
  * // Example usage:
  * // <GenreList />
- * 
+ *
  * @returns {JSX.Element} A list of genres with images and names.
  */
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+
+  if (error) return null;
+  if (isLoading) {
+    return (
+      <VStack colorPalette="teal">
+        <Spinner color="gray" />
+        <Text color="gray">Genres Loading...</Text>
+      </VStack>
+    );
+  }
 
   return (
     <List.Root listStyleType={"none"}>
@@ -27,7 +45,7 @@ const GenreList = () => {
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             ></Image>
-            <Text fontSize="lg" >{genre.name}</Text>
+            <Text fontSize="lg">{genre.name}</Text>
           </HStack>
         </ListItem>
       ))}
