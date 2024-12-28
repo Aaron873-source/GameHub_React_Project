3f082c9113cc0e3a325b94f23f7d86ee347d8b23
Aaron Mubiruan <mubiruaron2003@gmail.com>
2024-12-28 00:05:58 -0400

    Handled games with no image by making a default no image placeholder

--- game-hub/src/services/image-url.ts
+++ game-hub/src/services/image-url.ts
@@ -1,15 +1,20 @@
 //Added this component to optimize the image loading process by cropping the image URL to the desired size
 //so that users on a slow connection do not take too long to load the images since initially, the images are loaded in full size
 
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp";
 const getCroppedImageUrl = (url: string) => {
-    if (!url) return '';
+  if (!url) return noImage;
 
-    const target = "/media/";
-    const index = url.indexOf(target);
+  const target = "/media/";
+  const index = url.indexOf(target);
 
-    if (index === -1) return url;
+  if (index === -1) return url;
 
-    return url.slice(0, index + target.length) + "crop/600/400/" + url.slice(index + target.length);
+  return (
+    url.slice(0, index + target.length) +
+    "crop/600/400/" +
+    url.slice(index + target.length)
+  );
 };
 
 export default getCroppedImageUrl;
