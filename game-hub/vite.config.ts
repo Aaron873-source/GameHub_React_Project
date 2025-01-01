9d4c1dbe0a8266786603033cef6850433b4cc6d0
Aaron Mubiruan <mubiruaaron2003@gmail.com>
2025-01-01 16:09:22 -0400

    GitHubPages_Deploy_V1

--- game-hub/vite.config.ts
+++ game-hub/vite.config.ts
@@ -1,18 +1,15 @@
-import { defineConfig,loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from "vite-tsconfig-paths"
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ mode }) => {
-
   const env = loadEnv(mode, process.cwd());
 
   return {
     define: {
-      'process.env': env,
+      "process.env": env,
     },
     plugins: [react(), tsconfigPaths()],
     base: "/GameHub_React_Project/",
   };
-
-
-})
+});
